4fbf11164992edb3d25c15552a2b651982e5c4f4
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2021-02-10 21:51:52 +0100

    Add a RelayCommand

---
+++ WarehouseManager.UI/Commands/RelayCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace WarehouseManager.UI.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameters)
+        {
+            return _canExecute?.Invoke(parameters) ?? true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public void Execute(object parameters)
+        {
+            _execute(parameters);
+        }
+    }
+}
