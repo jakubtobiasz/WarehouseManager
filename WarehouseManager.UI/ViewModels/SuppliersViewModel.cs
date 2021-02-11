d70a7ac5665a10c5029d2f113d2e2514f1d720b4
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2021-02-11 18:51:17 +0100

    Update SuppliersViewModel name

--- WarehouseManager.UI/ViewModels/SuppliersViewModel.cs
+++ WarehouseManager.UI/ViewModels/SuppliersViewModel.cs
@@ -4,6 +4,6 @@ namespace WarehouseManager.UI.ViewModels
 {
     class SuppliersViewModel : IPageViewModel
     {
-        public string Name => "Home";
+        public string Name => "Suppliers";
     }
 }
