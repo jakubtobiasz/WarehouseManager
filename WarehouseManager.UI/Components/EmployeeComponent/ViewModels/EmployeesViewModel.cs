ebcd976c87dec363171e217c3ecc584785db1cf9
Jakub Tobiasz <jakub.tobiasz@icloud.com>
2021-02-14 15:58:49 +0100

    Prevent from deleting a employee managing a warehouse

--- WarehouseManager.UI/Components/EmployeeComponent/ViewModels/EmployeesViewModel.cs
+++ WarehouseManager.UI/Components/EmployeeComponent/ViewModels/EmployeesViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 using WarehouseManager.UI.Commands;
@@ -142,6 +143,12 @@ namespace WarehouseManager.UI.Components.EmployeeComponent.ViewModels
 
         private void RemoveEmployee(EmployeeModel employee)
         {
+            if (employee.ManagedWarehouses?.Any() ?? false)
+            {
+                MessageBox.Show("Pracownik nie może zostać usunięty jeśli zarządza jakimś magazynem!");
+                return;
+            }
+
             var messageBoxResult = MessageBox.Show($"Czy na pewno chcesz usunąć pracownika {employee.FirstName} {employee.LastName}?",
                 "Usuwanie pracownika", MessageBoxButton.YesNo);
 
